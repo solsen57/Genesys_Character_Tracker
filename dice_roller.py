5f912e566e7b6bfa07feba51c6f58c5313b5fa60
Sophie <sophieolsen57@outlook.com>
2022-04-16 19:42:43 -0500

    testing importing roller definitions into main script

--- dice_roller.py
+++ dice_roller.py
@@ -1,1 +1,12 @@
 #define dice and set up fucntions for rollers (force, skill, initiative?)
+
+import random
+
+#define dice outcomes
+blue = ['Blank', 'Blank', '1S', '1S, 1A', '2A', '1A']
+green = ['Blank', '1S', '1S', '2S', '1A', '1A', '1S, 1A', '2A']
+yellow = ['Blank', '1S', '1S', '2S', '2S', '1A', '1S, 1A', '1S, 1A', '1S, 1A', '2A', '2A', 'GH']
+black = ['Blank', 'Blank', '1F', '1F', '1D', '1D']
+purple = ['Blank', '1F', '1D', '2F', '1D', '1D', '1F, 1D', '2D']
+red = ['Blank', '1F', '1F', '2F', '2F', '1D', '1D', '1F, 1D', '1F, 1D', '2D', '2D', 'BH']
+white = ['1B', '1B', '1B', '1B', '1B', '1B', '2B', '1L', '1L', '2L', '2L', '2L']
--- main_sheet.py
+++ main_sheet.py
@@ -1,9 +1,10 @@
 #hello this is a test to make a branch on github
+import dice_roller as dr
 
 #character sheet builder
 
 #dice roller
-
+print(dr.blue)
 #main functionality
 #set up character
 #roll checks
