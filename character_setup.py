bf10c25228cbf3b87d1eb5576adcabe67f1e6287
Sophie <sophieolsen57@outlook.com>
2022-04-17 14:22:53 -0500

    start character sheet creator

---
+++ character_setup.py
@@ -0,0 +1,9 @@
+#file to create text doc for character sheet
+
+cs_ver = input('Would you like to create a new character (y/n)? ').lower()
+
+#add error, yes/no lists, find way to exit back to main menu
+if cs_ver == 'y':
+    pass
+else:
+    exit()
