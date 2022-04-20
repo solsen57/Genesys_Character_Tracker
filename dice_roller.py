ef85a047f4fead7a4ab6c1c7aef9bbddb0926225
Sophie <sophieolsen57@outlook.com>
2022-04-19 22:55:44 -0500

    added funtions to print raw roll and net roll results

--- dice_roller.py
+++ dice_roller.py
@@ -25,33 +25,89 @@ def force_roll(fw = 1):
     f_result = roll(white, fw)
 
     #separate light side and dark side
-    dark = [x for x in f_result if 'B' in x]
-    c_dark = [int(x[0]) for x in dark]
-    light = [x for x in f_result if 'L' in x]
-    c_light = [int(x[0]) for x in light]
+    dark = [int(x[0]) for x in f_result if 'B' in x]
+    light = [int(x[0]) for x in f_result if 'L' in x]
 
     print('Results of force dice: ')
-    if sum(c_dark) > 0:
-        print(f'{sum(c_dark)} dark side.')
-    if sum(c_light) > 0:
-        print(f'{sum(c_light)} light side.')
+    if sum(dark) > 0:
+        print(f'{sum(dark)} dark side.')
+    if sum(light) > 0:
+        print(f'{sum(light)} light side.')
 
     return f_result
 
-#translate results
-def results_text(list):
-    pass
+#translate net results
+def results_text(good_v, good_n, good_h, bad_v, bad_n, bad_h):
+    #compute results
+    vant = sum(good_v) - sum(bad_v)
+    norm = sum(good_n) - sum(bad_n)
+    hype = sum(good_h) - sum(bad_h)
+
+    #print results
+    res_strings = []
+    if (vant == 0) and (norm == 0) and (hype == 0):
+        print('It was a wash! Net zero!')
+    else:
+        if vant < 0:
+            res_strings.append(f'{abs(vant)} disadvantage')
+        elif vant > 0:
+            res_strings.append(f'{abs(vant)} advantage')
+        if norm < 0:
+            res_strings.append(f'{abs(norm)} failure')
+        elif norm > 0:
+            res_strings.append(f'{abs(norm)} success')
+        if hype < 0:
+            res_strings.append(f'{abs(hype)} DISPAIR')
+        elif hype > 0:
+            res_strings.append(f'{abs(hype)} TRIUMPH')
+    print('Results of Roll: ')
+    print(*res_strings, sep = ', ')
+    return vant, norm, hype
+
+#print roll results, not the net results
+def roll_text(list):
+    res_strings = []
+    #good results
+    good_v = [int(x[0]) for x in list if 'A' in x]
+    good_n = [int(x[0]) for x in list if 'S' in x]
+    good_h = [1 for x in list if 'GH' in x]
+
+    #bad results
+    bad_v = [int(x[0]) for x in list if 'D' in x]
+    bad_n = [int(x[0]) for x in list if 'F' in x]
+    bad_h = [1 for x in list if 'BH' in x]
+
+
+    if (sum(good_v) != 0):
+        res_strings.append(f'{sum(good_v)} advantage')
+    if (sum(good_n) != 0):
+        res_strings.append(f'{sum(good_n)} success')
+    if (sum(good_h) != 0):
+        res_strings.append(f'{sum(good_h)} TRIUMPH')
+    if (sum(bad_v) != 0):
+        res_strings.append(f'{sum(bad_v)} disadvantage')
+    if (sum(bad_n) != 0):
+        res_strings.append(f'{sum(bad_n)} failure')
+    if (sum(bad_h) != 0):
+        res_strings.append(f'{sum(bad_h)} DISPAIR')
+
+    print('Roll: ')
+    print(*res_strings, sep = ', ')
+    return good_v, good_n, good_h, bad_v, bad_n, bad_h
 
 #initiative roller
 def init_roll(cha = 1, rk = 0):
     if rk == 0:
         res = roll(green, cha)
+        roll_text(res)
         return res
     elif (cha == rk) & (rk != 0):
         res = roll(yellow, cha)
+        roll_text(res)
         return res
     else:
         upped = min(cha, rk)
         nor = max(cha, rk) - upped
         res = roll(yellow, upped) + roll(green, nor)
+        roll_text(res)
         return res
