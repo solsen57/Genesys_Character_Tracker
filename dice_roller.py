3dc7da8a62ad5c0b5d1d60e98d7eabc4912de2ab
Sophie <sophieolsen57@outlook.com>
2022-04-19 20:03:39 -0500

    added force and initiative rollers

--- dice_roller.py
+++ dice_roller.py
@@ -19,3 +19,49 @@ def roll(color, number):
         result.append(random.choice(color))
         i += 1
     return result
+
+#force dice roller
+def force_roll(fw = 1):
+    f_result = roll(white, fw)
+
+    #separate light side and dark side
+    dark = [x for x in f_result if 'B' in x]
+    c_dark = [int(x[0]) for x in dark]
+    light = [x for x in f_result if 'L' in x]
+    c_light = [int(x[0]) for x in light]
+
+    print('Results of force dice: ')
+    if sum(c_dark) > 0:
+        print(f'{sum(c_dark)} dark side.')
+    if sum(c_light) > 0:
+        print(f'{sum(c_light)} light side.')
+
+    return f_result
+
+#translate results
+def results_text(list):
+    pass
+
+#initiative roller
+def init_roll(cha = 1, rk = 0):
+    if rk == 0:
+        res = roll(green, cha)
+        print('1')
+        print(res)
+        return res
+    elif (cha == rk) & (rk != 0):
+        res = roll(yellow, cha)
+        print('2')
+        print(res)
+        return res
+    else:
+        upped = min(cha, rk)
+        nor = max(cha, rk) - upped
+        res = roll(yellow, upped) + roll(green, nor)
+        print('3')
+        print(upped)
+        print(nor)
+        print(res)
+        return res
+
+init_roll(4, 1)
